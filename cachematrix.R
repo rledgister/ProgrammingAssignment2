fb80f8490467ee27297e1d2177c74001300b0a4a
Roger Ledgister <roger.ledgister@gmail.com>
2020-07-01 16:59:15 -0700

    Completed assignment, creataed makeCacheMatrix and cacheSolve functions, comments complete.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are written to maintain the inverse of a given matrix,
+## allowing it to be generated once and then held in memory rather than recreated.
 
-## Write a short comment describing this function
+## This function memoizes a matrix object with simple functions to get or set its
+## inverse. Here, in comparison to the example, I have used a little more
+## descriptive naming for variables.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invmat <- NULL
+    set <- function(input){
+        x <<- input
+        invmat <<- NULL
+    }
+    get <- function () x
+    setmat <- function (mat) invmat <<- mat
+    getmat <- function () invmat
+    list(set = set, get = get, setmat = setmat, getmat = getmat)
 }
 
 
-## Write a short comment describing this function
+## This function takes a memoized matrix and either returns its solved inverse
+## or produces the inverse solution, while also storing that inverse in the
+## given matrix object.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("Reticulating Splines")
+        return(inv)
+    }
+    dat <- x$get()
+    x$setinv(solve(dat)) #Memoize this
+    x$getinv()
 }
